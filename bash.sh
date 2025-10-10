72f37db67c652cb8a6928d754346d42f17cfa250
chaozhang <chaozhang2000@outlook.com>
2025-10-11 01:24:39 +0800

    x

--- bash.sh
+++ bash.sh
@@ -1,2 +1,3 @@
 dot -Tpng soft_open_arch1.dot -o soft_open_arch1.png
-git add . && git commit -m "x" && git push origin main:main
+eog soft_open_arch1.png
+#git add . && git commit -m "x" && git push origin main:main
