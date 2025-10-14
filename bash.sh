84429af1e3120257fe1bf3009f1500cf71daaa6e
chaozhang <chaozhang2000@outlook.com>
2025-10-15 01:29:45 +0800

    x

--- bash.sh
+++ bash.sh
@@ -1,5 +1,8 @@
 dot -Tpng soft_open_arch1.dot -o ./result/soft_open_arch1.png
 dot -Tpng ./parts/definition.dot -o ./result/definition.png
 dot -Tpng ./parts/face_overview.dot -o ./result/face_overview.png
-eog ./result/soft_open_arch1.png
+dot -Tpng ./parts/MOSA_law.dot -o ./result/MOSA_law.png
+dot -Tpng ./parts/DOD.dot -o ./result/DOD.png
+#eog ./result/soft_open_arch1.png
+#eog ./result/DOD.png
 #git add . && git commit -m "x" && git push origin main:main
