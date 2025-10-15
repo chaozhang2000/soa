7db4eb23822d5eab294985aa8a2da88022680a2c
chaozhang <chaozhang2000@outlook.com>
2025-10-15 16:02:28 +0800

    x

--- bash.sh
+++ bash.sh
@@ -1,8 +1,8 @@
-dot -Tpng soft_open_arch1.dot -o ./result/soft_open_arch1.png
-dot -Tpng ./parts/definition.dot -o ./result/definition.png
-dot -Tpng ./parts/face_overview.dot -o ./result/face_overview.png
-dot -Tpng ./parts/MOSA_law.dot -o ./result/MOSA_law.png
-dot -Tpng ./parts/DOD.dot -o ./result/DOD.png
+#dot -Tpng soft_open_arch1.dot -o ./result/soft_open_arch1.png
+#dot -Tpng ./parts/definition.dot -o ./result/definition.png
+#dot -Tpng ./parts/face_overview.dot -o ./result/face_overview.png
+#dot -Tpng ./parts/MOSA_law.dot -o ./result/MOSA_law.png
+#dot -Tpng ./parts/DOD.dot -o ./result/DOD.png
 dot -Tpng ./parts/soa_feature_benifits.dot -o ./result/soa_feature_benifits.png
 #eog ./result/soft_open_arch1.png
 #eog ./result/DOD.png
